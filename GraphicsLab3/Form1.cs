e7dd32ef9b4b08c6b912e84ae6b0079af3707e6c
mrRomanovsky <rmansorokin@gmail.com>
2018-09-28 11:45:54 +0300

    implemented fulfilling inside a border

--- GraphicsLab3/Form1.cs
+++ GraphicsLab3/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,27 @@ namespace GraphicsLab3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void fillColor(Bitmap image, Point rowStart, Point rowEnd, Color col)
+        {
+            int y = rowStart.Y;
+            int x1 = rowStart.X;
+            int x2 = rowEnd.X;
+            for (int x = x1+1 ; x < x2; ++x)
+                image.SetPixel(x, y, col);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap image = (Bitmap)Bitmap.FromFile(textBox1.Text);
+            List<Point> border = new List<Point>(); // TODO: get border from image
+            border = border.OrderBy(p => p.Y).ThenBy(p => p.X).ToList();
+            for (int i = 0; i < border.Count - 1; ++i)
+            {
+                if (border[i].Y == border[i+1].Y)
+                    fillColor(image, border[i], border[i+1], Color.Blue);
+            }
         }
     }
 }
